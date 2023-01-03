f4a0cd3351d4ac56d32145dc5650dc065b9cedf4
gabino mendez <gabino_mendez@hotmail.com.ar>
2023-01-02 23:44:41 -0300

    terminado el hero,aun falta pulir responsive

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# hero-utn
+# Ejercicio de hero section para la diplomatura Fullstack de la UTN(universidad tecnologica Argentina)
 Hero section para ejercicio de la utn(universidad tecnológica Argentina)
---
+++ scripts/index.js
@@ -0,0 +1,19 @@
+let numero1 = parseInt(prompt("ingrese numero"));
+let numero2 = parseInt(prompt("ingrese numero 2"));
+let numero3 = parseInt(prompt("ingrese numero 3"));
+
+//operador circuito corto
+
+let avanzar = true
+
+avanzar && console.log('usted puede avanzar')
+
+// operador logico o ||
+
+if (condition) {
+  
+} else if (condition) {
+  
+}{
+  
+}
